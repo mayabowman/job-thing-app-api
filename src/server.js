08211457fa375254f790d37d16c1d2099bf08476
Maya Bowman <mayashady1@gmail.com>
2020-01-14 17:11:43 -0500

    working on deploy

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-// app.get('/api/*', (req, res) => {
-//   res.json({ok: true});
-// });
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
