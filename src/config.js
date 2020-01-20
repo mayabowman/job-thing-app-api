8f32b8df1a5b1633bbf1c71d73ace0262503a321
Maya Bowman <mayashady1@gmail.com>
2020-01-20 10:17:54 -0500

    switched config for deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/job-thing',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
-  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://job-thing-app.mayabowman.now.sh/",
+  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://job-thing-app.mayabowman.now.sh/",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
