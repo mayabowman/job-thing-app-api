bf1bb9c23457c7e35144a1a40b6ff3690d98722b
Maya Bowman <mayashady1@gmail.com>
2020-01-21 09:06:03 -0500

    fixed seed file

--- seeds/seed.job_thing_jobs.sql
+++ seeds/seed.job_thing_jobs.sql
@@ -2,10 +2,10 @@ INSERT INTO job_thing_jobs (user_id, company, position, status, description, dat
   VALUES
     (1, 'IBM', 'Front End Developer', 'Application submitted', 'Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.', '11/20/2019'),
     (1, 'Red Hat', 'Back End Developer', 'Application submitted', 'Override the digital divide with additional clickthroughs from DevOps.', '12/24/2019'),
-    (1, 'Cisco', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '/11/01/2019'),
+    (1, 'Cisco', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '11/01/2019'),
     (2, 'IQVIA', 'Front End Developer', 'Application submitted', 'Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.', '11/20/2019'),
     (2, 'AllScripts', 'Back End Developer', 'Application submitted', 'Override the digital divide with additional clickthroughs from DevOps.', '12/24/2019'),
-    (2, 'SAS', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '/11/01/2019'),
+    (2, 'SAS', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '11/01/2019'),
     (3, 'AdWorks', 'Front End Developer', 'Application submitted','Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.', '11/20/2019'),
     (3, 'Blue Matrix', 'Back End Developer', 'Application submitted', 'Override the digital divide with additional clickthroughs from DevOps.', '12/24/2019'),
-    (3, 'Pendo', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '/11/01/2019')
+    (3, 'Pendo', 'Full Stack Developer', 'Application submitted', 'Leverage agile frameworks to provide a robust synopsis for high level overviews.', '11/01/2019')
